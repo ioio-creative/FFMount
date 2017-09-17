3aacd2d792a5438e4fa190994c2f5f4719b10e49
ivanckt <123123@gmail.com>
2017-09-17 15:22:34 +0800

    add more timeline functions
    added new functions:
      - float getTimelineValue(int id, float t); //get the timeline value at speicfic time
      - bool getIsKeyframe(int id, float t); //return if the speific time is a keyframe
    added indicator lines for time in timeline.
    added keyframe enter event

--- TimelinePlayerControl/src/Keyframe.h
+++ TimelinePlayerControl/src/Keyframe.h
@@ -3,12 +3,12 @@
 #ifndef __ParkYohoKeyframe__
 #define __ParkYohoKeyframe__
 
-
 class Keyframe
 {
 public:
 	Keyframe();
 
+	int timelineId;
 	bool selected; //a state whethter this keyframe is selected by user 
 	float val; //value of this keyframe
 	float x; //x position of the keyframe
