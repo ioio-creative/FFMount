ed4c608a2eb8be4f0e90d11c807e0f8a7494ec11
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2017-09-25 14:03:07 +0800

    testing

--- Arduino/Test/MultipleSteppers_chris/MultipleSteppers_chris.ino
+++ Arduino/Test/MultipleSteppers_chris/MultipleSteppers_chris.ino
@@ -12,26 +12,31 @@ void setup() {
   Serial.begin(9600);
   
   stepper1.setMaxSpeed(MAX_SPEED);
-  stepper1.setAcceleration(computeAcceleration(15000, 20000));
+  stepper1.setAcceleration(computeAcceleration(20000, 20000));
   stepper1.moveTo(15000);
   
   stepper2.setMaxSpeed(MAX_SPEED);
   stepper2.setAcceleration(computeAcceleration(10000, 20000));
   stepper2.moveTo(10000);
+
+//  stepper1.runToPosition();
+//  Serial.println("Stepper 1 stopped at " + String(millis(), DEC));
+//  stepper2.runToPosition();
+//  Serial.println("Stepper 2 stopped at " + String(millis(), DEC));
 }
 
 void loop() {
-//  if (stepper1.distanceToGo() == 0 && !isStepper1Stopped)
-//  {
-//    Serial.println("Stepper 1 stopped at " + String(millis(), DEC));    
-//    isStepper1Stopped = true;
-//  }
-//
-//  if (stepper2.distanceToGo() == 0 && !isStepper2Stopped)
-//  {
-//    Serial.println("Stepper 2 stopped at " + String(millis(), DEC));
-//    isStepper2Stopped = true;
-//  }
+  if (stepper1.distanceToGo() == 0 && !isStepper1Stopped)
+  {
+    Serial.println("Stepper 1 stopped at " + String(millis(), DEC));    
+    isStepper1Stopped = true;
+  }
+
+  if (stepper2.distanceToGo() == 0 && !isStepper2Stopped)
+  {
+    Serial.println("Stepper 2 stopped at " + String(millis(), DEC));
+    isStepper2Stopped = true;
+  }
   
   stepper1.run();
   stepper2.run();
