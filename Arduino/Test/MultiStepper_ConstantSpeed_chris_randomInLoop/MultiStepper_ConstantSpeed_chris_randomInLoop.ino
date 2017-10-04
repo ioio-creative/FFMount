46aa37d95764c65b73a643e755a30f6d35e605fe
Kenny Wong <kennyviperhk@gmail.com>
2017-10-04 10:17:59 +0800

    upload

--- Arduino/Test/MultiStepper_ConstantSpeed_chris_randomInLoop/MultiStepper_ConstantSpeed_chris_randomInLoop.ino
+++ Arduino/Test/MultiStepper_ConstantSpeed_chris_randomInLoop/MultiStepper_ConstantSpeed_chris_randomInLoop.ino
@@ -3,7 +3,7 @@
 //MyStepper stepper1(MyStepper::DRIVER, 3, 2);
 //MyStepper stepper2(MyStepper::DRIVER, 9, 8);
 
-MyStepper stepper1(MyStepper::DRIVER, 7, 6);
+MyStepper stepper1(MyStepper::DRIVER, 3, 2);
 MyStepper stepper2(MyStepper::DRIVER, 9, 8);
 
 const int timeStep = 1000;  // ms
@@ -21,6 +21,8 @@ const long stepper2DistanceToMove = 32000;
 const long stepper2TimeToComplete = 30;  // s
 bool isStepper2PrintTimeStepToSerial = false;
 bool isStepper2Stopped = false;
+
+bool invertNum = true;
 
 void setup()
 {
@@ -43,24 +45,42 @@ long interval = 1000;           // interval at which to blink (milliseconds)
 
 void loop()
 {
-//  unsigned long currentMillis = millis();
+  //  unsigned long currentMillis = millis();
 
-//  if (currentMillis - previousMillis > interval) {
-//    previousMillis = currentMillis;
-//    
-//    Serial.print("distanceToGo : ");
-//    Serial.println(stepper1.distanceToGo());
-//  }
-    
+  //  if (currentMillis - previousMillis > interval) {
+  //    previousMillis = currentMillis;
+  //
+  //    Serial.print("distanceToGo : ");
+  //    Serial.println(stepper1.distanceToGo());
+  //  }
+  int c = -1;
+
   if (!stepper1.isCompleteTotDist())
   {
     stepper1.myRun();
   }
   else {
-    Serial.println("Stepper 1 stopped");
-    stepper1.reset(random(5000,6000), random(2, 8));    
+
+    if (invertNum) {
+      c = -1;
+    } else {
+      c = 1;
+    }
+    
+    invertNum = !invertNum;
+    
+    Serial.println("Curr Pos : ");
+    Serial.println(stepper1.currentPosition());
+    int a = c * random(5000, 6000);
+    int b = random(3, 5);
+    Serial.print(" a : ");
+    Serial.print(a);
+    Serial.print(" b : ");
+    Serial.println(b);
+
+    stepper1.reset(a, b);
   }
-  
+
   /*
     else if (!isStepper1Stopped)
     {
@@ -71,14 +91,14 @@ void loop()
     }
   */
 
-//  if (!stepper2.isCompleteTotDist())
-//  {
-//    stepper2.myRun();
-//  }
-//  else if (!isStepper2Stopped)
-//  {
-//    stepper2.printStatusToSerial();
-//    Serial.println("Stepper 2 stopped");
-//    isStepper2Stopped = true;
-//  }
+  //  if (!stepper2.isCompleteTotDist())
+  //  {
+  //    stepper2.myRun();
+  //  }
+  //  else if (!isStepper2Stopped)
+  //  {
+  //    stepper2.printStatusToSerial();
+  //    Serial.println("Stepper 2 stopped");
+  //    isStepper2Stopped = true;
+  //  }
 }
